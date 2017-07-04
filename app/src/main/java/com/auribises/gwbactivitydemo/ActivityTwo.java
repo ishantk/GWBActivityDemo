0609fb9b38970b5f6713533ea5e6fd85079de8eb
Ishant Kumar <er.ishant@gmail.com>
2017-07-04 16:01:00 +0530

    Initial commit

---
+++ app/src/main/java/com/auribises/gwbactivitydemo/ActivityTwo.java
@@ -0,0 +1,20 @@
+package com.auribises.gwbactivitydemo;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class ActivityTwo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two);
+    }
+
+    public void goBackHandler(View view){
+        finish();
+    }
+}
