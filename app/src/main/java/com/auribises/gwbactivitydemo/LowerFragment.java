d7e84d2fd06e981f0911037c956419743566e35c
Ishant Kumar <er.ishant@gmail.com>
2017-07-14 16:23:50 +0530

    Fragments in Android

--- app/src/main/java/com/auribises/gwbactivitydemo/LowerFragment.java
+++ app/src/main/java/com/auribises/gwbactivitydemo/LowerFragment.java
@@ -23,7 +23,8 @@ public class LowerFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_lower, container, false);
+        View view =  inflater.inflate(R.layout.fragment_lower, container, false);
+        return view;
     }
 
 }
--- app/src/main/java/com/auribises/gwbactivitydemo/UpperFragment.java
+++ app/src/main/java/com/auribises/gwbactivitydemo/UpperFragment.java
@@ -1,29 +1,54 @@
 package com.auribises.gwbactivitydemo;
 
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class UpperFragment extends Fragment {
+public class UpperFragment extends Fragment implements View.OnClickListener{
 
 
-    public UpperFragment() {
-        // Required empty public constructor
-    }
+    //@InjectView(R.id.button12)
+    Button btnSubmit;
 
+    Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_upper, container, false);
+        View view =  inflater.inflate(R.layout.fragment_upper, container, false);
+
+        //ButterKnife.inject(view);
+
+        context = getContext();
+
+        btnSubmit = (Button)view.findViewById(R.id.button12);
+        btnSubmit.setOnClickListener(this);
+
+        return view;
     }
 
+    void showNotification(){
+        NotificationManager notificationManager = (NotificationManager)context.getSystemService(context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //Toast.makeText(getActivity(),"You Clicked Button",Toast.LENGTH_LONG).show();
+        Toast.makeText(getContext(),"You Clicked Button",Toast.LENGTH_LONG).show();
+    }
 }
